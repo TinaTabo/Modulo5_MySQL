5dd1182bd544cc38ba13db2d51fc21fea42487d7
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-20 13:06:03 +0200

    API REST preparada para programar

---
+++ RetosTema4/src/routers/alumnos.routers.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const router = Router();
+const alumnosCtrl = require("../controller/alumnos.controller");
+
+
+module.exports = router;
---
+++ RetosTema4/src/routers/notas.routers.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const router = Router();
+const notasCtrl = require("../controller/notas.controller");
+
+
+module.exports = router;
